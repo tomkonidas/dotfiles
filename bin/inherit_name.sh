c9ba12fd3732b29aa3e18c6077cd31b52d7f2850
Tom Konidas <tomkonidas@protonmail.ch>
2020-02-07 22:49:18 -0500

    Add recusrive function

--- bin/inherit_name.sh
+++ bin/inherit_name.sh
@@ -3,24 +3,25 @@
 # This script is meant to rename each file in a directory
 # with it's parent's directory's name.
 
-DIRS=$(ls -d */)
-
-for DIR in $DIRS
-do
-  dir($DIR)
-done
-
-dir(CUR)
-do
-  cd $CUR
+function dir() {
+  cd $1
     for FILE in $(ls)
     do
-      if [ -d $CUR ]
+      if [ -d $FILE ]
       then
-        $(dir($CUR))
+        $(dir $FILE )
       elif [ -f $FILE ]
+      then
         echo $FILE
       fi
     done
   cd ..
+}
+
+DIRS=$(ls -d */)
+
+for DIR in $DIRS
+do
+  dir $DIR
 done
+
