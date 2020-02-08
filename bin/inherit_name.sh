442cbfdfb180aaed248fbf61c48f4e206f398893
Tom Konidas <tomkonidas@protonmail.ch>
2020-02-07 21:55:23 -0500

    Add loops over dir

--- bin/inherit_name.sh
+++ bin/inherit_name.sh
@@ -2,3 +2,19 @@
 
 # This script is meant to rename each file in a directory
 # with it's parent's directory's name.
+
+DIRS=$(ls -d */)
+
+for DIR in $DIRS
+do
+  cd $DIR
+  for FILE in $(ls)
+  do
+    if [ -f $FILE ]
+    then
+      echo $FILE
+    fi
+  done
+  cd ..
+done
+
