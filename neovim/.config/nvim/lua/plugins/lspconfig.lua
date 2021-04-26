e81cf5630f536bceb063a9d75a7213e770d5bd71
Tom Konidas <tomkonidas@protonmail.ch>
2021-04-26 10:20:39 -0400

    Add more LSP languages

--- neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -4,11 +4,35 @@ require'lspconfig'.elixirls.setup{
 }
 
 -- HTML
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local html_capabilities = vim.lsp.protocol.make_client_capabilities()
+html_capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 require'lspconfig'.html.setup {
-  capabilities = capabilities,
+  capabilities = html_capabilities,
  }
 
 require'lspconfig'.tsserver.setup{}
+
+local css_capabilities = vim.lsp.protocol.make_client_capabilities()
+css_capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.cssls.setup {
+  capabilities = css_capabilities,
+}
+
+require'lspconfig'.stylelint_lsp.setup{
+  settings = {
+    stylelintplus = {
+      autoFixOnFormat = true,
+      autoFixOnSave = true,
+      validateFixOnFormat = true,
+      validateFixOnSave = true
+    }
+  }
+}
+
+require'lspconfig'.bashls.setup{}
+
+require'lspconfig'.yamlls.setup{}
+
+require'lspconfig'.dockerls.setup{}
