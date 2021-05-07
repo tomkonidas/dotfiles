16668ea6395bb1fb7926b536da4aac16edd81d4c
Tom Konidas <tomkonidas@protonmail.ch>
2021-05-06 21:07:29 -0400

    Add elixir tree-sitter

--- neovim/.config/nvim/lua/plugins/treesiter.lua
+++ neovim/.config/nvim/lua/plugins/treesiter.lua
@@ -20,3 +20,13 @@ require'nvim-treesitter.configs'.setup {
     enable = true
   }
 }
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.elixir = {
+  install_info = {
+    url = "https://github.com/wingyplus/tree-sitter-elixir",
+    files = {"src/parser.c"},
+    branch = "main"
+  }
+}
