11c93b045ebb65ffbce1a6b2287dd0dab01f1dbe
Tom Konidas <tomkonidas@protonmail.ch>
2022-05-03 17:59:30 -0400

    Remove unused plugs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,46 +24,9 @@ return require("packer").startup(
             config = [[require('config.treesitter')]]
         }
 
-        use {
-            "kdheepak/tabline.nvim",
-            config = function()
-                require "tabline".setup {
-                    -- Defaults configuration options
-                    enable = true,
-                    options = {
-                        -- If lualine is installed tabline will use separators configured in lualine by default.
-                        -- These options can be used to override those settings.
-                        section_separators = {"", ""},
-                        component_separators = {"", ""},
-                        max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-                        show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
-                        show_devicons = true, -- this shows devicons in buffer section
-                        show_bufnr = false, -- this appends [bufnr] to buffer section,
-                        show_filename_only = true, -- shows base filename only instead of relative path in filename
-                        modified_icon = "+ ", -- change the default modified icon
-                        modified_italic = false -- set to true by default; this determines whether the filename turns italic if modified
-                    }
-                }
-                vim.cmd [[
-                  set guioptions-=e " Use showtabline in gui vim
-                  set sessionoptions+=tabpages,globals " store tabpages and globals in session
-                ]]
-            end,
-            requires = {{"hoob3rt/lualine.nvim", opt = true}, {"kyazdani42/nvim-web-devicons", opt = true}}
-        }
-
         use {"chaoren/vim-wordmotion", event = "BufEnter"}
 
         use {"rrethy/nvim-base16", event = "VimEnter"}
-
-        use {
-            "hoob3rt/lualine.nvim",
-            requires = {
-                "kyazdani42/nvim-web-devicons",
-                opt = true
-            },
-            config = [[require('config.lualine')]]
-        }
 
         use {
             "nvim-telescope/telescope.nvim",
